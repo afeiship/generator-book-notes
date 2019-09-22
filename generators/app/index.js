9faa1f9d8f69afea9d30c41344d6530efe99fe20
afeiship <1290657123@qq.com>
2019-09-22 22:36:12 +0800

    add: intail

---
+++ generators/app/index.js
@@ -0,0 +1,36 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const glob = require("glob");
+const { resolve } = require("path");
+const remote = require("yeoman-remote");
+const yoHelper = require("yeoman-generator-helper");
+const replace = require("replace-in-file");
+
+module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(
+        `Welcome to the stunning ${chalk.red("generator-book-notes")} generator!`
+      )
+    );
+    this.composeWith("github", this.props);
+  }
+
+  writing() {
+    const done = this.async();
+    remote(
+      "afeiship",
+      "boilerplate-book-notes",
+      function(err, cachePath) {
+        // copy files:
+        this.fs.copy(
+          glob.sync(resolve(cachePath, "{**,.*}")),
+          this.destinationPath()
+        );
+        done();
+      }.bind(this)
+    );
+  }
+};
